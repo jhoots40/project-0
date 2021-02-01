e18d27b7824dbd576cbeb63422f667cab6427b57
Jake Houts <jhoots40@iastate.edu>
2021-02-01 10:28:08 -0600

    Finished creating the 23x23 sandpile

--- project-0.c
+++ project-0.c
@@ -2,15 +2,15 @@
 
 int main(int argc, char *argv[]) 
 {
-   int table[23][23];
+   int table[23][23] = {0};
 	
    int i, j;
 	
-   for (j = 0; j < 23; j++){
+   for (j = 0; j < 23; j++) {
       for (i = 0; i < 23; i++) {
          printf("%d ", table[j][i]);
       }
       printf("\n");
    }
-	return 0;
+   return 0;
 }
